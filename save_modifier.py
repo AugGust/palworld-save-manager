0266344443e1f9976c9d579fc5aedaf09fb1383e
Jy <piggy013130@yahoo.com.sg>
2024-02-06 13:39:47 +0800

    Speed up by doing conversions in memory only

--- save_modifier.py
+++ save_modifier.py
@@ -1,6 +1,6 @@
-import json
 import os
 from convert import convert_json_to_sav, convert_sav_to_json
+import copy
 
 level_json_cached = None
 
@@ -9,7 +9,6 @@ def rename_player(save_path, player_guid, new_name):
 
 	guid_formatted = '{}-{}-{}-{}-{}'.format(player_guid[:8], player_guid[8:12], player_guid[12:16], player_guid[16:20], player_guid[20:]).lower()
 	level_sav_path = save_path + '/Level.sav'
-	level_json_path = level_sav_path + '.json'
 
 	# save_path must exist in order to use it.
 	if not os.path.exists(save_path):
@@ -28,18 +27,10 @@ def rename_player(save_path, player_guid, new_name):
 					character["value"]["RawData"]["value"]["object"]["SaveParameter"]["value"]["NickName"]["value"] = new_name
 					break
 
-	with open(level_json_path, 'w') as f:
-		json.dump(level_json_cached, f, indent=2)
-	print('JSON file have been exported')
+	level_json = copy.deepcopy(level_json_cached)
+	convert_json_to_sav(level_json, level_sav_path)
 	
-	# Convert our JSON files to save files.
-	json_to_sav(level_json_path)
-	print('Converted JSON files back to save file')
-	
-	# Clean up JSON file which is no longer needed.
-	os.remove(level_json_path)
-	print('Miscellaneous file removed')
-	
+	print('Converted JSON back to save file')	
 	print('Renamed! Have fun!')
 	return True
 
@@ -47,7 +38,6 @@ def update_host_meta(save_path):
 	global level_json_cached
 
 	meta_sav_path = save_path + '/LevelMeta.sav'
-	meta_json_path = meta_sav_path + '.json'
 
 	# save_path must exist in order to use it.
 	if not os.path.exists(save_path):
@@ -77,28 +67,14 @@ def update_host_meta(save_path):
 	if not new_host_name:
 		return
 	
-	sav_to_json(meta_sav_path)
+	meta_json = convert_sav_to_json(meta_sav_path)
 	print('Converted meta file to JSON')
-	
-	# Parse our JSON files.
-	with open(meta_json_path) as f:
-		meta_json = json.load(f)
-	print('JSON file have been parsed')
 
 	meta_json["properties"]["SaveData"]["value"]["HostPlayerName"]["value"] = new_host_name
 	meta_json["properties"]["SaveData"]["value"]["HostPlayerLevel"]["value"] = new_host_level
 
-	with open(meta_json_path, 'w') as f:
-		json.dump(meta_json, f, indent=2)
-	print('JSON file have been exported')
-	
-	# Convert our JSON files to save files.
-	json_to_sav(meta_json_path)
-	print('Converted JSON file back to save file')
-	
-	# Clean up JSON files which are no longer needed.
-	os.remove(meta_json_path)
-	print('Miscellaneous file removed')
+	convert_json_to_sav(meta_json, meta_sav_path)
+	print('Converted JSON back to save file')
 
 def replace_save_file(save_path, new_guid, old_guid):
 	global level_json_cached
@@ -110,9 +86,6 @@ def replace_save_file(save_path, new_guid, old_guid):
 	level_sav_path = save_path + '/Level.sav'
 	old_sav_path = save_path + '/Players/'+ old_guid + '.sav'
 	new_sav_path = save_path + '/Players/' + new_guid + '.sav'
-	level_json_path = level_sav_path + '.json'
-	old_json_path = old_sav_path + '.json'
-	new_json_path = new_sav_path + '.json'
 	
 	# save_path must exist in order to use it.
 	if not os.path.exists(save_path):
@@ -129,16 +102,9 @@ def replace_save_file(save_path, new_guid, old_guid):
 		return False
 
 	# Convert save files to JSON so it is possible to edit them.
-	sav_to_json(old_sav_path)
-	sav_to_json(new_sav_path)
+	old_json = convert_sav_to_json(old_sav_path)
+	new_json = convert_sav_to_json(new_sav_path)
 	print('Converted save files to JSON')
-	
-	# Parse our JSON files.
-	with open(old_json_path) as f:
-		old_json = json.load(f)
-	with open(new_json_path) as f:
-		new_json = json.load(f)
-	print('JSON files have been parsed')
 	
 	# Replace all instances of the old GUID with the new GUID.
 	
@@ -180,23 +146,10 @@ def replace_save_file(save_path, new_guid, old_guid):
 					group_id["value"]["RawData"]["value"]["players"][j]["player_uid"] = new_guid_formatted
 	print('Changes have been made')
 	
-	# Dump modified data to JSON.
-	with open(old_json_path, 'w') as f:
-		json.dump(old_json, f, indent=2)
-	with open(level_json_path, 'w') as f:
-		json.dump(level_json_cached, f, indent=2)
-	print('JSON files have been exported')
-	
-	# Convert our JSON files to save files.
-	json_to_sav(level_json_path)
-	json_to_sav(old_json_path)
-	print('Converted JSON files back to save files')
-	
-	# Clean up JSON files which are no longer needed.
-	os.remove(level_json_path)
-	os.remove(old_json_path)
-	os.remove(new_json_path)
-	print('Miscellaneous files removed')
+	convert_json_to_sav(old_json, old_sav_path)
+	level_json = copy.deepcopy(level_json_cached)
+	convert_json_to_sav(level_json, level_sav_path)
+	print('Converted JSON back to save files')
 	
 	# We must rename the patched save file from the old GUID to the new GUID for the server to recognize it.
 	if os.path.exists(new_sav_path):
@@ -215,9 +168,6 @@ def swap_save_file(save_path, a_guid, b_guid):
 	a_sav_path = save_path + '/Players/' + a_guid + '.sav'
 	b_sav_path = save_path + '/Players/'+ b_guid + '.sav'
 	temp_sav_path = save_path + '/Players/temp.sav'
-	level_json_path = level_sav_path + '.json'
-	a_json_path = a_sav_path + '.json'
-	b_json_path = b_sav_path + '.json'
 	
 	# save_path must exist in order to use it.
 	if not os.path.exists(save_path):
@@ -238,17 +188,10 @@ def swap_save_file(save_path, a_guid, b_guid):
 		return False
 
 	# Convert save files to JSON so it is possible to edit them.
-	sav_to_json(a_sav_path)
-	sav_to_json(b_sav_path)
+	a_json = convert_sav_to_json(a_sav_path)
+	b_json = convert_sav_to_json(b_sav_path)
 	print('Converted save files to JSON')
-	
-	# Parse our JSON files.
-	with open(a_json_path) as f:
-		a_json = json.load(f)
-	with open(b_json_path) as f:
-		b_json = json.load(f)
-	print('JSON files have been parsed')
-	
+
 	# Replace all instances of the old GUID with the new GUID.
 	
 	# Player data replacement.
@@ -288,27 +231,12 @@ def swap_save_file(save_path, a_guid, b_guid):
 				elif group_id["value"]["RawData"]["value"]["players"][j]["player_uid"] == b_new_guid_formatted:
 					group_id["value"]["RawData"]["value"]["players"][j]["player_uid"] = a_new_guid_formatted
 	print('Changes have been made')
-	
-	# Dump modified data to JSON.
-	with open(a_json_path, 'w') as f:
-		json.dump(a_json, f, indent=2)
-	with open(b_json_path, 'w') as f:
-		json.dump(b_json, f, indent=2)
-	with open(level_json_path, 'w') as f:
-		json.dump(level_json_cached, f, indent=2)
-	print('JSON files have been exported')
-	
-	# Convert our JSON files to save files.
-	json_to_sav(level_json_path)
-	json_to_sav(a_json_path)
-	json_to_sav(b_json_path)
-	print('Converted JSON files back to save files')
-	
-	# Clean up miscellaneous JSON files which are no longer needed.
-	os.remove(level_json_path)
-	os.remove(a_json_path)
-	os.remove(b_json_path)
-	print('Miscellaneous files removed')
+
+	convert_json_to_sav(a_json, a_sav_path)
+	convert_json_to_sav(b_json, b_sav_path)
+	level_json = copy.deepcopy(level_json_cached)
+	convert_json_to_sav(level_json, level_sav_path)
+	print('Converted JSON back to save files')
 	
 	# Swap the file names
 	os.rename(a_sav_path, temp_sav_path)
@@ -317,18 +245,9 @@ def swap_save_file(save_path, a_guid, b_guid):
 	print('Profiles have been swapped! Have fun!')
 	return True
 
-def sav_to_json(file):
-	convert_sav_to_json(file, file + ".json", False)
-
-def json_to_sav(file: str):
-	convert_json_to_sav(file, file.replace(".json", ""))
-
 def get_player_list(file, cacheJson: bool = True):
 	global level_json_cached
-	level_json_path = file + ".json"
-	sav_to_json(file)
-	with open(level_json_path) as f:
-		level_json = json.load(f)
+	level_json = convert_sav_to_json(file)
 	players = []
 	characters = level_json["properties"]["worldSaveData"]["value"]["CharacterSaveParameterMap"]["value"]
 	for character in characters:
@@ -336,7 +255,7 @@ def get_player_list(file, cacheJson: bool = True):
 			if (character["value"]["RawData"]["value"]["object"]["SaveParameter"]["value"]["IsPlayer"]["value"]):
 				players.append((character["key"]["PlayerUId"]["value"].replace('-','').upper(), character["value"]["RawData"]["value"]["object"]["SaveParameter"]["value"]["NickName"]["value"]))
 
-	os.remove(level_json_path)
 	if cacheJson:
 		level_json_cached = level_json
+
 	return players
